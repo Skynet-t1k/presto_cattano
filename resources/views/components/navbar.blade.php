305ac05dbd35ca354440606079ae434a19efcacc
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-07-24 09:52:30 +0200

    start progetto scaffolding installato

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-secondary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/"><img src="./img/prestologo.png" class="shadow" alt="presto logo" width="120" height="60"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mb-2 mb-lg-0 d-flex justify-content-around w100">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="/">Home</a>
+          </li>
+
+          <li class="nav-item dropdown d-flex">
+            <a class="nav-link dropdown-toggle border rounded-0 border-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Categorie
+            </a>
+            <ul class="dropdown-menu p-0 rounded-0 border-0">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            <form class="d-flex p-0 s-input" role="search">
+              <input class="form-control rounded-0 border-0" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn s-btn border-0" type="submit"><i class="fa-solid fa-magnifying-glass" style="color: #000000;"></i></button>
+            </form>
+             </li>
+             <li class="nav-item">
+                <a class="nav-link" href="#">Accedi</a>
+            </li>
+            
+            
+           </ul>
+        </div>
+    </div>
+  </nav>
