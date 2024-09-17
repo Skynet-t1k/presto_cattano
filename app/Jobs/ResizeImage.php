97f7d01bdd40afe0f7e89c15d0149a71edd580fe
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-09-17 15:18:20 +0200

    completata user story 9

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Illuminate\Bus\Queueable;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Queue\SerializesModels;
@@ -39,6 +40,14 @@ class ResizeImage implements ShouldQueue
 
         Image::load($srcPath)
             ->crop($w, $h, CropPosition::Center)
+            ->watermark(
+                base_path('resources/img/watermark.png'),
+                width: 150,
+                height: 150,
+                paddingX: 5,
+                paddingY: 5,
+                paddingUnit: Unit::Percent
+            )
             ->save($destPath);
     }
 }
